4cac38d5ce69ea154a0eb5f072c168e4bdd2c3db
Maksim Horbachevsky <fantactuka@gmail.com>
2015-01-27 09:34:27 +0300

    Minor refactoring

--- app/initializers/ember-action-with-event.js
+++ app/initializers/ember-action-with-event.js
@@ -1,21 +1,21 @@
 import Ember from 'ember';
 
 export default {
-  name: 'action-with-event',
+  name: 'ember-action-with-event',
   initialize: function() {
 
     // Override {{action}} helper so that the last argument being sent to the controller/router
     // is original event object
     var ActionHelper = Ember.Handlebars.ActionHelper,
-      originalRegisterAction = Ember.Handlebars.ActionHelper.registerAction;
+      originalRegisterAction = ActionHelper.registerAction;
 
-    Ember.Handlebars.ActionHelper.registerAction = function(actionNameOrPath, options) {
+    ActionHelper.registerAction = function(actionNameOrPath, options) {
       var actionId = originalRegisterAction.apply(this, arguments),
         originalRegisteredActionHandler = ActionHelper.registeredActions[actionId].handler;
 
       ActionHelper.registeredActions[actionId].handler = function handleRegisteredAction(event) {
         options.parameters.push(event);
-        originalRegisteredActionHandler.call(this, event);
+        return originalRegisteredActionHandler.apply(this, arguments);
       };
 
       return actionId;
